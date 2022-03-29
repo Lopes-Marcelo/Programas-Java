c8099c1db46cb0d8c40cf58be79175f959453378
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-03-29 18:44:37 -0400

    1

--- Exercícios Livro/src/Retangulo.java
+++ Exercícios Livro/src/Retangulo.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Retangulo {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Digite o valor da altura do retângulo: ");
+        float alt = teclado.nextFloat();
+        System.out.print("Digite o valor do comprimento do retângulo: ");
+        float com = teclado.nextFloat();
+        float area = alt * com;
+        float perimetro = (alt * 2) + (com * 2);
+        System.out.println("O valor da área é " + area);
+        System.out.print("O valor do perímetro é " + perimetro);
     }
 }
