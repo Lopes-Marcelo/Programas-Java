81e05f077ce88b5ba3990888af4154f04fb55af5
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-03-29 18:28:31 -0400

    1

--- Exercícios Livro/src/Retangulo.java
+++ Exercícios Livro/src/Retangulo.java
@@ -1,5 +1,5 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Marcelo!");
+public class Retangulo {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
 }
