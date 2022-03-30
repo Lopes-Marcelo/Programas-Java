e31cd660a81849966be9d240b8ab2a6f48cf40c4
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-03-30 14:56:42 -0400

    13

---
+++ Exercícios Livro/src/NumeroExp.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class NumeroExp {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Digite um número: ");
+        int n1 = teclado.nextInt();
+        System.out.print("Digite outro número: ");
+        int n2 = teclado.nextInt();
+        int quadrado1 =  (int) Math.pow(n1,n2);
+        int quadrado2 = (int) Math.pow(n2,n1);
+        System.out.println(n1 + " elevado a " + n2 + " é igual a: " + quadrado1);
+        System.out.println(n2 + " elevado a " + n1 + " é igual a: " + quadrado2);
+    }
+    
+}
