e8a4d3820ff13b7d4cb58dc35e70b018dd708fa9
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-03-30 12:15:52 -0400

    10

---
+++ Exercícios Livro/src/AreaTriangulo.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class AreaTriangulo {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Informe a altura do triângulo em cm: ");
+        int altura = teclado.nextInt();
+        System.out.print("Informe a base do trinângulo em cm: ");
+        int base = teclado.nextInt();
+        double area = (base * altura)/2;
+        System.out.println("A área do triângulo informado é: " + area + " cm².");
+    }    
+}
