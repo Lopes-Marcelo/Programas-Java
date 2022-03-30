ffb9e9cfe5be587e3e1cb4ebad3671b8ba2ec87b
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-03-30 10:58:19 -0400

    6

---
+++ Exercícios Livro/src/ReajusteVariavel.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ReajusteVariavel {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Informe seu salário: ");
+        double salario = teclado.nextDouble();
+        System.out.print("Informe a porcentagem do reajuste: ");
+        double reajuste = teclado.nextDouble();
+        double novo_reajuste = ((reajuste/100) + 1) * salario;
+        System.out.println("Salário informado: R$" + salario);
+        System.out.println("Reajuste informado: " + reajuste + " %.");
+        System.out.println("O salário com o reajuste será de R$" + novo_reajuste);
+
+    }
+}
