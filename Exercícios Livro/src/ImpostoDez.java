3a2a53b4b43163e06d5828bcca2f5877bbb9e80e
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-03-30 11:19:27 -0400

    8

--- Exercícios Livro/src/ImpostoDez.java
+++ Exercícios Livro/src/ImpostoDez.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class ImpostoDez {
     public static void main(String[] args) {
-        
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Informe seu salário: ");
+        double salario = teclado.nextDouble();
+        System.out.print("Você possui R$50,00 de gratificação no salário final e 10% de imposto no valor do salário base.");
+        double salario2 = salario * 0.9;
+        double salario3 = 50 + salario2;
+        System.out.println("Salário informado: R$" + salario);
+        System.out.println("O salário com os reajustes será de R$" + salario3);    
     }    
 }
