3c7829c9136bd20eebf7027ef387b5ec00dc51b5
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-03-30 12:02:58 -0400

    9

---
+++ Exercícios Livro/src/DepositoJuros.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class DepositoJuros {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Informe a a quantia a ser depositada: R$" );
+        double deposito = teclado.nextDouble();
+        System.out.println("A taxa para depósitos é de 3% do valor total.");
+        double total = deposito * 1.03;
+        System.out.println("O valor total da operação é de: R$" + total);
+    }
+    
+}
