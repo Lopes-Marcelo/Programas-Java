379507cfa444943fd6fe60795a464b4db723399b
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-04-05 17:11:08 -0400

    25

---
+++ Exercícios Livro/Estrutura Sequencial/src/Teatro.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Teatro {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int custo, convite, qtd;
+        System.out.print("Informe o custo do espetáculo: R$");
+        custo = teclado.nextInt();
+        System.out.println("Informe o valor do convite individual: R$");
+        convite = teclado.nextInt();
+        qtd = custo/convite;
+        System.out.println("O espetáculo precisa de " + qtd + " vendas de convite, no mínimo.");
+        teclado.close();
+    }
+    
+}
