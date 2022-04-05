56e48e4dc1b3735a83ad7a67bc577ce39543e83d
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-04-05 16:02:41 -0400

    21

---
+++ Exercícios Livro/Estrutura Sequencial/src/Quadro.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Quadro{
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        // x = altura que deseja pregar o quadro
+        // y = distância em que deverá ficar a escada
+        // z = tamanho da escada
+        System.out.print("Informe o tamanho da escada em metros: ");
+        double z = teclado.nextDouble();
+        System.out.print("Informe a altura que deseja pregar o quadro: ");
+        double x = teclado.nextDouble();
+        double y = (z * z) - (x * x);
+        double y2 = Math.sqrt(y);
+        System.out.println("A distância da escada e da parede é de " + y2);
+        teclado.close();
+
+        //Exercício 21 
+    }
+}
