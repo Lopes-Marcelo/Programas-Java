20d68dfce83c70675e447b61391214202497dbf5
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-04-05 11:40:06 -0400

    21

---
+++ Exercícios Livro/src/Escada.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Escada {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Informe a altura da parede em metros: ");
+        double alt = teclado.nextDouble();
+        System.out.print("Informe o ângulo formado em graus: ");
+        double ang = teclado.nextDouble();
+        double radiano = (ang * 3.14) /180;
+        double escada = (alt)/ Math.sin(radiano);
+        System.out.println("A medida da escada é de " + escada + " metros.");
+    }
+    
+}
