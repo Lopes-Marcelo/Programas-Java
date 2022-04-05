fb1c6cbb40637197af15d6f3c3656728b374053c
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-04-05 11:55:48 -0400

    20

--- Exercícios Livro/src/Degrau.java
+++ Exercícios Livro/src/Degrau.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class Degrau {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        System.out.print("Informe a altura do ");
+        System.out.print("Informe a altura do degrau em metros: ");
+        double alt_deg = teclado.nextDouble();
+        System.out.print("Informe a altura requerida pelo usuário: ");
+        double alt_usu = teclado.nextDouble();
+        double qnt_deg = Math.ceil(alt_usu / alt_deg);
+        System.out.println("A quantidade de degraus necessária será de: " + qnt_deg + ".");
+        teclado.close();
     }
     
 }
