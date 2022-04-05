effc9e453945b414dab01aced10ada8982bcdcef
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-04-05 16:19:11 -0400

    22

---
+++ Exercícios Livro/Estrutura Sequencial/src/Energia.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Energia {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        float vlr_sal, qtd_kw, vlr_kw, vlr_reais, desc, vlr_desc;
+        System.out.print("Informe o valor do salário mínimo: R$" );
+        vlr_sal = teclado.nextFloat();
+        System.out.print("Informe a quantidade de quilowatts consumida: ");
+        qtd_kw = teclado.nextFloat();
+        vlr_kw = vlr_sal / 5;
+        vlr_reais = vlr_kw * qtd_kw;
+        desc = (vlr_reais * 15) / 100;
+        vlr_desc = vlr_reais - desc;
+        System.out.println("O valor de cada quilowatt é de R$" + vlr_kw);
+        System.out.println("O valor a ser pago pela residência é de R$" + vlr_reais);
+        System.out.println("O valor com desconto a ser pago é de R$" + vlr_desc);
+        teclado.close();
+
+        // Exercício 22
+    }
+}
