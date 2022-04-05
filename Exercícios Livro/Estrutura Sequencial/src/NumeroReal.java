23f1b5a9ea6b8f1f2c410f43dd07bf6b09b54c7b
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-04-05 16:49:49 -0400

    23

---
+++ Exercícios Livro/Estrutura Sequencial/src/NumeroReal.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class NumeroReal {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        double num, i , f, a;
+        System.out.print("Digite um número real e positivo qualquer: ");
+        num = teclado.nextDouble();
+        i = Math.floor(num); 
+        f = num / 100;
+        a = Math.ceil(num);
+        System.out.println("O valor inteiro desse número é " + i);
+        System.out.println("O valor fracionário desse número é " + f);
+        System.out.println("O valor arredondado desse número é " + a);
+        teclado.close();
+    }
+    
+}
