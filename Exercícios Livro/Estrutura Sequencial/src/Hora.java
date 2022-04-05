1f4b987f630b6ec258e92218097cff8cc1705c1f
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-04-05 17:03:10 -0400

    24

---
+++ Exercícios Livro/Estrutura Sequencial/src/Hora.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Hora {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        double hora, h, m , conversao;
+        System.out.print("Informe o tempo em horas/minutos: ");
+        hora = teclado.nextDouble();
+        h = Math.floor(hora);
+        m = hora - h;
+        conversao = (h * 60) + (m * 100);
+        System.out.println("O total de minutos diante do valor apresentado é " + conversao + " minutos.");
+        teclado.close();
+    }
+    
+}
