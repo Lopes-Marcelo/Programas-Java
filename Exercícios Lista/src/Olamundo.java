601a71dca334c3e092b3a41c00a34226527cab11
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-04-06 11:12:14 -0400

    1

--- Exercícios Lista/src/Olamundo.java
+++ Exercícios Lista/src/Olamundo.java
@@ -1,7 +1,8 @@
 public class Olamundo {
     public static void main(String[] args) throws Exception {
         System.out.println("Olá, Mundo!");
+
+        //Exercício 1
+        /* Escreva um programa que mostre na tela a mensagem "Olá, Mundo!"*/
     }
 }
-
-/* Escreva um programa que mostre na tela a mensagem "Olá, Mundo!"*/
