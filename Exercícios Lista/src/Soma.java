88f3fb1ac781c7f4a56f2b16c76662caf443cdfb
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-04-06 11:16:05 -0400

    4

---
+++ Exercícios Lista/src/Soma.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Soma {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Digite um número inteiro qualquer: ");
+        int n1 = teclado.nextInt();
+        System.out.print("Digite outro número inteiro qualquer: ");
+        int n2 = teclado.nextInt();
+        int soma = n1 + n2;
+        System.out.println("A soma dos valores digitados é " + soma);
+        teclado.close();
+
+        //Exercício 4
+        /*Desenvolva um algoritmo que leia dois números inteiros e mostre o somatório
+        entre eles.*/
+    }
+    
+}
