761e93d614fa1b4685a45fd0cdd1663fe4dc41a3
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-04-06 11:10:07 -0400

    3

--- Exercícios Lista/src/Salario.java
+++ Exercícios Lista/src/Salario.java
@@ -11,6 +11,8 @@ public class Salario {
         teclado.close();
 
         //Exercício 3 
+        /* Crie um programa que leia o nome e o salário de um funcionário, mostrando no
+        final uma mensagem.*/
     }
     
 }
