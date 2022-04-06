0626fcf291b1ddc0b9c33a4bab3ca4d552e229eb
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-04-06 11:19:43 -0400

    5

---
+++ Exercícios Lista/src/Notas.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Notas {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Informe a nota 1: ");
+        float nota1 = teclado.nextFloat();
+        System.out.println("Informe a nota 2: ");
+        float nota2 = teclado.nextFloat();
+        float media = (nota1 + nota2)/2;
+        System.out.println("A média do aluno é: " + media);
+        teclado.close();
+        
+        //Exercício 5 
+        /*Faça um programa que leia as duas notas de um aluno em uma matéria e mostre
+        na tela a sua média na disciplina.*/
+    }
+    
+}
