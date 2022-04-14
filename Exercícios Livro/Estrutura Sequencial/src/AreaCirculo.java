61a7829039a5d20eac2812cd4fd820cb2dbdcdd8
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-04-13 20:57:56 -0400

    11

--- Exercícios Livro/Estrutura Sequencial/src/AreaCirculo.java
+++ Exercícios Livro/Estrutura Sequencial/src/AreaCirculo.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class AreaCirculo {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        System.out.print("Informe o raio do círculo em cm: ");
+        System.out.print("Informe o raio do círculo (em cm): ");
         int raio = teclado.nextInt();
         double area = (raio * raio) * 3.14;
         System.out.println("A área do cícurlo é: " + area + " cm². (Considerando PI = 3,14).");
