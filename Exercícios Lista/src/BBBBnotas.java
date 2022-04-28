f5c442289af5b136217c1eefc40f8a0f5a906b82
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-04-28 13:55:02 -0400

    5

--- Exercícios Lista/src/BBBBnotas.java
+++ Exercícios Lista/src/BBBBnotas.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Notas {
+public class BBBBnotas {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         System.out.println("Informe a nota 1: ");
