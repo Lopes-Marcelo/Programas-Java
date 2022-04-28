0533d5843e7a13df898622d9a9a0d08c833d8b9e
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-04-28 13:54:38 -0400

    4

--- Exercícios Lista/src/Asoma.java
+++ Exercícios Lista/src/Asoma.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Soma {
+public class Asoma {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         System.out.print("Digite um número inteiro qualquer: ");
