179d52b1fe0f56ce4993383083878965ae8abb69
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-04-29 00:08:53 -0400

    7

--- Exercícios Lista/src/BBdobro.java
+++ Exercícios Lista/src/BBdobro.java
@@ -7,7 +7,7 @@ public class BBdobro {
         int n1 = teclado.nextInt();
         int dobro = n1 * 2;
         int terca = n1 / 3;
-        System.out.println("O dobro do número digitado é " + dobro + " e a terça parte desse número é " + terca);
+        System.out.println("O dobro do número digitado é igual a " + dobro + " e a terça parte desse número é " + terca);
         teclado.close();
 
         //Crie um algoritmo que leia um número real e mostre na tela o seu dobro e a sua terça parte.
