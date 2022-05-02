eb70f0a87c53a10073894892027a129243b27393
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-05-02 11:01:42 -0400

    tools - resolução do sistema

--- Exemplos/src/Aresolucaosistema.java
+++ Exemplos/src/Aresolucaosistema.java
@@ -4,6 +4,6 @@ public class Aresolucaosistema {
     public static void main(String[] args) {
         //Tool tk = Tool.getDefaultToolkit();
         //Dimension d = tk.getScreenSize();
-        System.out.println("A resolução da sua máquina é: " +d.width + " x " +d.height);
+        //System.out.println("A resolução da sua máquina é: " +d.width + " x " +d.height);
     }
 }
