4bc4bc8942f6860c09e5b371edf4e274beda3897
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-05-02 10:52:39 -0400

    print simples

---
+++ Exemplos/src/AAoixau.java
@@ -0,0 +1,7 @@
+public class AAoixau {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Oi!");
+        System.out.println("\n");
+        System.out.println("Xau");
+    }
+}
