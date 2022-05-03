429fae284240ac3a5fdf59944e5714a718a4667d
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-05-03 11:23:06 -0400

    operadores - lógicos

---
+++ Exemplos/src/Eoperadoreslogicos.java
@@ -0,0 +1,26 @@
+public class Eoperadoreslogicos {
+
+    public static void main(String[] args) {
+        /*int x = 3;
+        int y = 4;
+        int z = 2;
+        boolean r;
+        r = (x < y && y < z)? true:false;
+        System.out.println(r);*/
+
+        /*int x = 3;
+        int y = 4;
+        int z = 2;
+        boolean r;
+        r = (x < y || y < z)? true:false;
+        System.out.println(r);*/
+
+        int x = 3;
+        int y = 4;
+        int z = 2;
+        boolean r;
+        r = (x < y ^ y < z)? true:false;
+        System.out.println(r);
+    }
+    
+}
