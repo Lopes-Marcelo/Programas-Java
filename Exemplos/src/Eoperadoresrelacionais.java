8be063a69f5100348f284a5b186b135cfed48452
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-05-03 11:32:47 -0400

    operadores - relacionais1~

---
+++ Exemplos/src/Eoperadoresrelacionais.java
@@ -0,0 +1,21 @@
+public class Eoperadoresrelacionais {
+
+    public static void main(String[] args) {
+        
+        /*String nome1 = "Marcelo";
+        String nome2 = "Marceloo";
+        String res = (nome1 == nome2) ? "Igual":"Diferente";
+        System.out.println(res);*/
+
+        String nome1 = "Marcelo";
+        String nome3 = new String ("Marcelo");
+        String res = (nome1.equals(nome3)) ? "Igual":"Diferente";
+        System.out.println(res);
+        
+        // < "menor que"; > "maior que"; <= "menor igual a"; >= "maior igual a"; == "igual a"; != "diferente de"; 
+
+        //Em java, "=" é a atribuição.
+    }
+    
+}
+
