4478be975068dc08119d682575a1391b010dd997
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-05-03 11:37:35 -0400

    operadores - aritméticos

--- Exemplos/src/Doperadoresaritimeticos.java
+++ Exemplos/src/Doperadoresaritimeticos.java
@@ -1,9 +1,9 @@
 public class Doperadoresaritimeticos {
     public static void main(String[] args) {
-        /*int n1 = 3;
+        int n1 = 3;
         int n2 = 5;
         float media = (n1 + n2)/2;
-        System.out.println("A média entre 5 e 3 é " + media);*/
+        System.out.println("A média entre 5 e 3 é " + media);
 
         int numero = 5;
         //int valor = 5 + ++numero;
@@ -11,21 +11,21 @@ public class Doperadoresaritimeticos {
         System.out.println(valor);
         System.out.println(numero);
 
-        /*int x = 4;
+        /int x = 4;
         x += 2; // x = x + 2
-        System.out.println(x);*/
+        System.out.println(x);
 
-        /*int v =  5;
+        int v =  5;
         int ex = (int) Math.pow(v,2); // pi, sqrt ou cbrt
-        System.out.format("A raíz quadrada de 5 é " + ex);*/
+        System.out.format("A raíz quadrada de 5 é " + ex);
 
-        /*float v = 8.9f;
+        float v = 8.9f;
         int ar = (int) Math.floor(v); //ceil, round ou abs
-        System.out.println(ar);*/
+        System.out.println(ar);
 
-        /*double ale = Math.random();
+        double ale = Math.random();
         int n =(int) (2 + ale *(9 -2 ));
-        System.out.println(n);*/
+        System.out.println(n);
     }
 }
 
