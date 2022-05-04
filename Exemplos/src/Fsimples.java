82bc615b3c35e8ab26aed1a0c7d9cf5768403ae7
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-05-04 01:03:19 -0400

    estrutura condicional - simples

--- Exemplos/src/Fsimples.java
+++ Exemplos/src/Fsimples.java
@@ -3,14 +3,16 @@ import java.util.Scanner;
 public class Fsimples {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
+        System.out.println("Informe a sua primeira nota: ");
         float n1 = teclado.nextFloat();
+        System.out.println("Informe a sua segunda nota: ");
         float n2 = teclado.nextFloat();
         float m = (n1 + n2)/2;
-        System.out.println("Sua média é igual a " + m);
+        System.out.println("A sua média é igual a " + m);
         teclado.close();
 
         if (m > 7) {
-            System.out.println("Parabéns");
+            System.out.println("Parabéns, você passou!");
         }
         
         //Estrutura condicional simples
