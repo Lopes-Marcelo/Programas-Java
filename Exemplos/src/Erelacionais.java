4f14574242e018f7f3272e76315d278dc1fcee20
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-05-05 00:08:07 -0400

    operadores - relacionais

---
+++ Exemplos/src/Erelacionais.java
@@ -0,0 +1,23 @@
+public class Erelacionais {
+
+    public static void main(String[] args) {
+        
+        String nome1 = "Marcelo";
+        String nome2 = "Marceloo";
+        String res = (nome1 == nome2) ? "Igual":"Diferente";
+        System.out.println(res);
+
+        String nome4 = "Marcelo";
+        String nome3 = new String ("Marcelo");
+        String resp = (nome4.equals(nome3)) ? "Igual":"Diferente";
+        System.out.println(resp);
+        
+        
+        // < "menor que"; > "maior que"; <= "menor igual a"; >= "maior igual a"; == "igual a"; != "diferente de"; 
+
+        // Em java, "=" é a atribuição.
+
+        // Operadores Relacionais
+    }
+    
+}
