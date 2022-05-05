f1c86c994380dea6b71bb36f74d854b51a3b1db9
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-05-05 00:06:44 -0400

    operadores - ternários

---
+++ Exemplos/src/Eoperadoresternarios.java
@@ -0,0 +1,16 @@
+public class Eoperadoresternarios {
+    public static void main(String[] args) {
+        int n1 = 3;
+        int n2 = 4;
+        int maior = n1 > n2 ? n1 : n2;
+        System.out.println(maior);
+
+        int n3, n4, r;
+        n3 = 9;
+        n4 = 7;
+        r = n3 > n4 ? 0:1;
+        System.out.println(r);
+
+        //Ternário se define em: se não for um é o outro.
+    }
+}
