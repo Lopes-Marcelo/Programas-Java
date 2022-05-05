ca2dc007731328014f039956548c82e156aa0d8a
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-05-05 00:13:44 -0400

    tipos primitivos

---
+++ Exemplos/src/Ctiposprimitivos.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ctiposprimitivos {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner (System.in);
+         
+        System.out.print("Digite o nome do aluno: ");
+        String nomee = teclado.nextLine();
+        System.out.print("Digite a nota do aluno: ");
+        float notaa = teclado.nextFloat();
+        String nome = "Marcelo";
+        float nota = 8.5f;
+        System.out.println("Sua nota é " + nota);
+        System.out.printf("Sua nota é %.3f \n", nota);
+        System.out.format("A nota de %s é %.2f \n", nome, nota);
+        System.out.println("sua nota é " + notaa);
+        System.out.printf("sua nota é %.3f \n", notaa);
+        System.out.format("a nota de %s é %.3f \n", nomee, notaa);
+        teclado.close();
+
+        //Outros exemplos no Java são: byte;short;int;long;boolean;char;float;double
+
+        // Tipos Primitivos
+    }
+}
