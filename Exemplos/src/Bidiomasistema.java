e5a95be5e651363562e4398f7272c01f50d8de4c
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-05-06 00:04:05 -0400

    tools - idioma do sistema

--- Exemplos/src/Bidiomasistema.java
+++ Exemplos/src/Bidiomasistema.java
@@ -2,8 +2,11 @@ import java.util.Locale;
 
 public class Bidiomasistema {
     public static void main(String[] args) {
+        
         Locale loc = Locale.getDefault();
         System.out.print("O idioma do sistema é o ");
         System.out.println (loc.getDisplayLanguage()); // imprime “Português”
-    }
+
+        // Tools - idioma do sistema
+    }  
 }
