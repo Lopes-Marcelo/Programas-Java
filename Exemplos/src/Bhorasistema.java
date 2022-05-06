9bed8976ba481f95bf6b7281d9709bc9dddd8967
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-05-06 00:02:39 -0400

    tools - horário do sistema

--- Exemplos/src/Bhorasistema.java
+++ Exemplos/src/Bhorasistema.java
@@ -2,10 +2,12 @@ import java.util.Date;
 
 public class Bhorasistema {
     public static void main(String[] args) {
-        // Inicio
+        
         Date relogio = new Date();
         System.out.print("A hora do sistema é ");
         System.out.println(relogio.toString());
-        // Fim
+        
+
+        // Tools - horário do sistema
     }
 }
