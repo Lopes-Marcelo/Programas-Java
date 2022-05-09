f29627f7ff8b45c433cf7e3d279776e7903bb47e
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-05-09 13:56:34 -0400

    estrutura de repetição - for

---
+++ Exemplos/src/Gfor.java
@@ -0,0 +1,17 @@
+public class Gfor {
+    public static void main(String[] args) {
+        for (int cont = 0; cont <= 3; cont ++) {
+            System.out.println("Número " + cont);
+        }
+
+        for (int cont = 0; cont <= 10; cont += 2) { 
+            System.out.println("Número " + cont);
+        }
+
+        for (int cont = 15; cont >= 1; cont -= 1){
+            System.out.println("Número " + cont);
+        }
+
+        // Estrutura de repetição - for
+    }
+}
