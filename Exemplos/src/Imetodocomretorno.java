1377ae132aa89f8ad5261a4024fac945c81fbb42
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-05-10 00:06:18 -0400

    estrutura básica - método com retorno

---
+++ Exemplos/src/Imetodocomretorno.java
@@ -0,0 +1,15 @@
+public class Imetodocomretorno {
+
+    static int soma (int a, int b) {
+        int s = a + b;
+        return s;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("O programa começou!!!");
+        int sm = soma(5,2);
+        System.out.println("A soma vale " + sm);
+    }
+
+    // Estrutura básica de método com retorno.
+}
---
+++ Exemplos/src/Imetodosemretorno.java
@@ -0,0 +1,14 @@
+public class Imetodosemretorno {
+
+    static void soma (int a , int b){
+        int s = a + b;
+        System.out.println("A soma é " + s);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Começou o programa!");
+        soma (5,2);
+    }
+
+    // Estrutura de método sem retorno
+}
