48712a7782eb6aaa33773c76e2ee8061ad280c4b
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-05-10 23:32:14 -0400

    estrutura básica - método sem retorno

--- Exemplos/src/Imetodosemretorno.java
+++ Exemplos/src/Imetodosemretorno.java
@@ -7,7 +7,7 @@ public class Imetodosemretorno {
 
     public static void main(String[] args) {
         System.out.println("Começou o programa!");
-        soma (5,2);
+        soma (5,3);
     }
 
     // Estrutura básica de método sem retorno
