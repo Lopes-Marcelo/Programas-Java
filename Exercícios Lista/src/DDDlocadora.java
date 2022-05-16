7c054e432af8f8081886ab53fdf6b4e90b855e38
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-05-16 15:53:47 -0400

    14

--- Exercícios Lista/src/DDDlocadora.java
+++ Exercícios Lista/src/DDDlocadora.java
@@ -7,7 +7,7 @@ public class DDDlocadora {
         double sal_inicial = teclado.nextDouble();
         double sal_final = sal_inicial * 1.15;
         
-        System.out.println("O seu salário com o aumento será de R$" + sal_final);
+        System.out.println("O seu novo salário com o aumento será de R$" + sal_final);
         teclado.close();
         
         //Faça um algoritmo que leia o salário de um funcionário, calcule e mostre o seu novo salário, com 15% de aumento.
