3a57a76ad7cd7a266bf055112042698fa8246d4b
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-05-16 16:08:06 -0400

    14

--- Exercícios Lista/src/DDDlocadora.java
+++ Exercícios Lista/src/DDDlocadora.java
@@ -3,13 +3,15 @@ import java.util.Scanner;
 public class DDDlocadora {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        System.out.print("Informe o seu salário atual: R$");
-        double sal_inicial = teclado.nextDouble();
-        double sal_final = sal_inicial * 1.15;
+        System.out.print("Quantos quilômetros foram rodados com o veículo: ");
+        float km = teclado.nextFloat();
+        System.out.print("Por quantos dias o carro esteve alugado: ");
+        int dias_alugados = teclado.nextInt();
+        double preco_final = dias_alugados*90 + km*0.2;
         
-        System.out.println("O seu novo salário com o aumento será de R$" + sal_final);
+        System.out.println("O preço total da locação é de R$" + preco_final);
         teclado.close();
         
-        //Faça um algoritmo que leia o salário de um funcionário, calcule e mostre o seu novo salário, com 15% de aumento.
+        //Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preço total a pagar, sabendo que o carro custa R$90 por dia e R$0,20 por Km rodado.
     }
 }
