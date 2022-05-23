7e42e829f6f76ccb3fdec0a32892e99a3e854133
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-05-23 17:45:27 -0400

    40

--- Exercícios Lista/src/JJcontadorreverso.java
+++ Exercícios Lista/src/JJcontadorreverso.java
@@ -1,10 +1,10 @@
 public class JJcontadorreverso {
     public static void main(String[] args) {
-        for (int cont = 10; cont >= 3; cont -= 1){
+        for (int cont = 0; cont <= 18; cont += 3){
             System.out.println(cont);
         }
         System.out.println("Acabou!");
 
-        //Faça um algoritmo que mostre na tela a seguinte contagem: 10 9 8 7 6 5 4 3 Acabou!
+        //Crie um aplicativo que mostre na tela a seguinte contagem: 0 3 6 9 12 15 18 Acabou!
     }
 }
