55628efe07e3d3d028bda24bf68a2073253a3081
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-05-23 17:46:37 -0400

    40

--- Exercícios Lista/src/JcontadorPA.java
+++ Exercícios Lista/src/JcontadorPA.java
@@ -1,4 +1,4 @@
-public class JJcontadorreverso {
+public class JcontadorPA {
     public static void main(String[] args) {
         for (int cont = 0; cont <= 18; cont += 3){
             System.out.println(cont);
