20cb49e81a42f4df4cba92d06aa64520532d2c38
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-05-23 17:21:09 -0400

    38

---
+++ Exercícios Lista/src/JJJcontador.java
@@ -0,0 +1,11 @@
+public class JJJcontador {
+    public static void main(String[] args) {
+        int cont = 5;
+        while (cont < 10) {
+            cont ++;
+            System.out.println(cont);
+        }
+
+        //Escreva um programa que mostre na tela a seguinte contagem: 6 7 8 9 10 11 Acabou!
+    }
+}
