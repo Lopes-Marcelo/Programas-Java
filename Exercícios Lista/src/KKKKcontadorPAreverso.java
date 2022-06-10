555d2ffa50c8af38dcc47fa55406299afb379a64
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-06-10 15:02:03 -0400

    41

---
+++ Exercícios Lista/src/KKKKcontadorPAreverso.java
@@ -0,0 +1,11 @@
+public class KKKKcontadorPAreverso {
+    
+    public static void main(String[] args) {
+        for (int cont = 100; cont >= 0; cont -= 5){
+            System.out.println(cont);
+        }
+        System.out.println("Acabou!");
+
+        //Crie um programa que mostre na tela a seguinte contagem: 100 95 90 85 80 ... 0 Acabou!
+    }
+}
