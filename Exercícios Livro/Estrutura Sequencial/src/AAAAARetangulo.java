bf1d4f0ac78b75fc267ab811d93b645e065bdd98
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-06-10 14:54:01 -0400

    ok

--- Exercícios Livro/Estrutura Sequencial/src/AAAAARetangulo.java
+++ Exercícios Livro/Estrutura Sequencial/src/AAAAARetangulo.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Retangulo {
+public class AAAAARetangulo {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         System.out.print("Digite o valor da altura do retângulo: ");
